deab3ce5b0116d4ab9b85ace3031d6197de33c21
Theguilherm3 <guilherme789ts@gmail.com>
2026-01-20 14:00:08 -0300

    Correções finais, ultimas rotas adicionadas

--- schemas/transactions.py
+++ schemas/transactions.py
@@ -6,7 +6,7 @@ from models.transactions import EnumMovmentType, EnumPaymentTypes
 
 
 class TransactionCreate(BaseModel):
-    movment_type: EnumMovmentType
+    movment_type: EnumMovmentType = EnumMovmentType.SAIDA
     transaction_type: EnumPaymentTypes
     account_id: int
     amount: float = Field(gt=0, examples=[129.97])
