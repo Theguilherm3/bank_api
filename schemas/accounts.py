34915b02dc770b6812df7e9b25d1b60100c3cad2
Theguilherm3 <guilherme789ts@gmail.com>
2026-01-21 16:08:58 -0300

    Pytest finalizado, testes do router accounts 100% finalizados

--- schemas/accounts.py
+++ schemas/accounts.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class AccountCreate(BaseModel):
-    username: str = "Seu Nome"
+    username: str = Field(min_length=2)
     balance: float = Field(gt=0, default=5412)
 
 
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -4,8 +4,11 @@ from sqlalchemy.orm.session import Session
 from schemas.accounts import AccountCreate
 from tests.test_main import client
 
-
 # uv run -m pytest -v --cov=routes --cov-report term-missing
+# uv run -m pytest -v -s
+# uv run -m pytest -v
+
+
 def test_create_new_account(test_db: Session):
     account_data = AccountCreate(username="Marília", balance=254.25)
     response = client.post("/conta/criar", json=account_data.model_dump())
@@ -45,3 +48,21 @@ def test_invalid_payload(test_db, payload, status_code, detail):
     assert response.status_code == status_code
     print(response.text)
     assert detail in response.text
+
+
+def test_get_balance_valid_account(test_db):
+    account_data = AccountCreate(username="Marília", balance=254.25)
+    new_account = client.post("/conta/criar", json=account_data.model_dump())
+    account_number = new_account.json()["account_number"]
+
+    response = client.get("/conta", params={"account_number": account_number})
+    assert response.status_code == 200
+    assert "balance" in response.json()
+
+
+@pytest.mark.parametrize(
+    "account_number, status_code", [(5425, 404), ("qweq", 422), ("", 422), (None, 422)]
+)
+def test_get_balance_invalid_account(test_db, account_number, status_code):
+    response = client.get("/conta", params={"account_number": account_number})
+    assert response.status_code == status_code
